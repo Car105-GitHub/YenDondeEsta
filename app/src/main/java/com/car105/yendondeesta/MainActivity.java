b58490ddab3e5ebec725b0c25360aeb07b42a263
Car105 <car105@MacBook-Pro-de-Car105.local>
2021-11-11 21:08:41 -0500

    commit inicio del proyecto

---
+++ app/src/main/java/com/car105/yendondeesta/MainActivity.java
@@ -0,0 +1,21 @@
+package com.car105.yendondeesta;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void goToMap(View view){
+        Intent intentMap = new Intent(this,MapsActivity.class);
+        startActivity(intentMap);
+    }
+}
