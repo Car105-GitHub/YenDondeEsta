5237e36a91c4f14b32d178402817134459b02617
Carlos E Restrepo F <cerf_fx@yahoo.com>
2021-12-02 19:22:49 -0500

    Geolocalizacion de un celular en tiempo real

--- app/src/main/java/com/car105/yendondeesta/MapsActivity.java
+++ app/src/main/java/com/car105/yendondeesta/MapsActivity.java
@@ -1,13 +1,22 @@
 package com.car105.yendondeesta;
 
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.car105.yendondeesta.databinding.ActivityMapsBinding;
@@ -28,6 +37,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        getLocalizacion();
+    }
+
+    private void getLocalizacion() {
+        int permiso = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION);
+        if(permiso == PackageManager.PERMISSION_DENIED){
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)){
+            }else{
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            }
+        }
     }
 
     /**
@@ -39,18 +59,73 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng UTP = new LatLng(4.796070095768436, -75.69016710359328);
-        mMap.addMarker(new MarkerOptions().position(UTP).title("UTP Pereira"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(UTP));
-        mMap.animateCamera(
-                CameraUpdateFactory.newLatLngZoom(UTP, 16f),
-                4000,
-                null
-        );
+        /**     // Add a marker in Sydney and move the camera
+         LatLng UTP = new LatLng(4.796070095768436, -75.69016710359328);
+         mMap.addMarker(new MarkerOptions().position(UTP).title("UTP Pereira"));
+         mMap.moveCamera(CameraUpdateFactory.newLatLng(UTP));
+         mMap.animateCamera(
+         CameraUpdateFactory.newLatLngZoom(UTP, 16f),
+         4000,
+         null
+         );
+         }
+         }
+         */
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+
+        mMap.getUiSettings().setMyLocationButtonEnabled(false);
+
+        LocationManager locationManager = (LocationManager) MapsActivity.this.getSystemService(Context.LOCATION_SERVICE);
+        LocationListener locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                LatLng miUbicacion = new LatLng(location.getLatitude(), location.getLongitude());
+                mMap.addMarker(new MarkerOptions().position(miUbicacion).title("ubicacion actual"));
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(miUbicacion));
+                CameraPosition cameraPosition = new CameraPosition.Builder()
+                        .target(miUbicacion)
+                        .zoom(14)
+                        .bearing(90)
+                        .tilt(45)
+                        .build();
+                mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+
+
     }
 }
